f160ebbef9f8dbc2ccedde30e9980243e23779b8
Victor-Camilo6 <victor.c.nunes@edu.pbh.gov.br>
2025-06-03 10:29:09 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,38 +1,47 @@
 #include <stdio.h>
 
-int main() {
-    // Tabuleiro 10x10, cheio de 0 = tudo vazio no começo
-    int tabuleiro[10][10] = {0};
-
-    // NAVIO 1 - Vertical (tamanho 4) na coluna 2 (x = 2)
-    for (int i = 0; i < 4; i++) {
-        tabuleiro[i][2] = 3;
-    }
-
-    // NAVIO 2 - Horizontal (tamanho 5) na linha 6 (y = 6)
-    for (int i = 0; i < 5; i++) {
-        tabuleiro[6][i] = 3;
-    }
-
-    // NAVIO 3 - Diagonal principal (↘) começando em (1,1), tamanho 4
-    for (int i = 0; i < 4; i++) {
-        tabuleiro[1 + i][1 + i] = 3;
-    }
-
-    // NAVIO 4 - Diagonal secundária (↙) começando em (3,6), indo pra esquerda em baixo
-    for (int i = 0; i < 4; i++) {
-        tabuleiro[3 + i][6 - i] = 3;
-    }
-
-    // Agora vamos exibir o tabuleiro completo
-    printf("Tabuleiro Batalha Naval - Nível Aventureiro:\n\n");
-    for (int y = 0; y < 10; y++) {
-        for (int x = 0; x < 10; x++) {
-            printf("%d ", tabuleiro[y][x]);
+// Função para imprimir uma matriz qualquer (usada pra todas as habilidades)
+void imprimirMatriz(int linhas, int colunas, int matriz[linhas][colunas]) {
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d ", matriz[i][j]);
         }
-        printf("\n"); // quebra linha depois de cada linha do tabuleiro
+        printf("\n");
     }
+    printf("\n");
+}
+
+int main() {
+    // HABILIDADE: Cone (3 linhas x 5 colunas)
+    int cone[3][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1}
+    };
+
+    // HABILIDADE: Octaedro (3 linhas x 5 colunas)
+    int octaedro[3][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {0, 0, 1, 0, 0}
+    };
+
+    // HABILIDADE: Cruz (3 linhas x 5 colunas)
+    int cruz[3][5] = {
+        {0, 0, 1, 0, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 1, 0, 0}
+    };
+
+    // Exibindo as habilidades
+    printf("🔺 Habilidade: Cone\n");
+    imprimirMatriz(3, 5, cone);
+
+    printf("🔷 Habilidade: Octaedro\n");
+    imprimirMatriz(3, 5, octaedro);
+
+    printf("➕ Habilidade: Cruz\n");
+    imprimirMatriz(3, 5, cruz);
 
     return 0;
 }
-
